077287381927d3ed60949dbdf2c67f8a75b9a7b9
OmerF4ruk <omer_faruk18@outlook.com>
2021-05-31 00:31:32 +0300

    123123

--- src/main/java/northwind/kodlamaio/api/controllers/UsersController.java
+++ src/main/java/northwind/kodlamaio/api/controllers/UsersController.java
@@ -3,13 +3,17 @@ package northwind.kodlamaio.api.controllers;
 
 import northwind.kodlamaio.business.abstracts.UserService;
 import northwind.kodlamaio.core.entities.User;
-import northwind.kodlamaio.core.utilities.result.Result;
+import northwind.kodlamaio.core.utilities.result.ErrorDataResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value="/api/users")
@@ -23,8 +27,20 @@ public class UsersController {
     }
 
     @PostMapping(value="/add")
-    public ResponseEntity<?> add(@RequestBody User user){
+    public ResponseEntity<?> add(@Valid  @RequestBody User user){
         return  ResponseEntity.ok(this.userService.add(user));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleValidationExcepiton(MethodArgumentNotValidException excepitons){
+        Map<String,String> validationErrors = new HashMap<String ,String>();
+        for(FieldError fieldError : excepitons.getBindingResult().getFieldErrors()){
+            validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());
+        }
+
+        ErrorDataResult<Object> errors =new ErrorDataResult<Object>(validationErrors,"Doğrulama Hataları");
+        return errors;
     }
 
 }
